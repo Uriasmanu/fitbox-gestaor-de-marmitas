6c303e7f0954ce6f5a4db80ea9f7a3ab1f93be77
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-08-07 12:00:21 -0300

    Feat - Criação das primeiras rotas e paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 
+import { Route, Router, Routes } from 'react-router-dom'
 import './App.css'
+import Registro from './assets/Pages/Registros'
+import NotFound from './assets/Pages/NotFound'
+import Home from './assets/Pages/Home'
 
 function App() {
   
 
   return (
     <>
-      
+      <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Registro" element={<Registro />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
     </>
   )
 }
