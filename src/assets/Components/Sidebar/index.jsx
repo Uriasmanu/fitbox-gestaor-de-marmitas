a1db6a452983597903b4d4ed228433a8c4e9a832
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-08-07 13:00:42 -0300

    - feat: Estruturação do sidebar

--- src/assets/Components/Sidebar/index.jsx
+++ src/assets/Components/Sidebar/index.jsx
@@ -1,8 +1,28 @@
-import "./_sidebar.scss"
+import "./_sidebar.scss";
+import imagemUsuario from "../../Image/usuario.png"
+import texts from "../../Texts/text.json"
+import { FaUtensils, FaBox, FaShoppingCart, FaFileAlt, FaWarehouse } from 'react-icons/fa';
 
-const Sidebar = () =>{
-    return(
+const Sidebar = () => {
+    const iconMap = {
+        FaUtensils: <FaUtensils />,
+        FaBox: <FaBox />,
+        FaShoppingCart: <FaShoppingCart />,
+        FaFileAlt: <FaFileAlt />,
+        FaWarehouse: <FaWarehouse />
+      };
+    return (
         <div className="ContainerSidebar">
+            <img src={imagemUsuario} alt="Imagem do usuatio" />
+            <h2>Ola, {texts.sidebar.usuario}!</h2>
+            <ul>
+        {texts.sidebar.menus.map((menu, index) => (
+          <li key={index}>
+            <span className="icon">{iconMap[menu.icon]}</span>
+            {menu.label}
+          </li>
+        ))}
+      </ul>
         </div>
     )
 }
