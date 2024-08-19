597e1ff1342b6754eeac24450de4ec63cb380313
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-08-19 12:43:44 -0300

    feat: Finalizar estilização da Sidebar
    - Completar mapeamento de ícones com base no JSON de configuração.
    - Atualizar o componente Sidebar para exibir ícones correspondentes aos itens de menu.
    - Ajustar a exibição do nome do usuário e a estrutura da lista de menus.

--- src/assets/Components/Sidebar/index.jsx
+++ src/assets/Components/Sidebar/index.jsx
@@ -1,31 +1,43 @@
 import "./_sidebar.scss"
 import imagemUsuario from "../../Image/usuario.png"
 import texts from "../../Texts/text.json"
-import { FaUtensils, FaBox, FaShoppingCart, FaFileAlt, FaWarehouse } from 'react-icons/fa';
-import "../../Styles/_styleBase.scss"; 
+
+import "../../Styles/_styleBase.scss";
+
+import iconeAlimento from '../../Image/alimento.svg'
+import iconeCompra from '../../Image/compra.svg'
+import iconeEstoque from '../../Image/estoque.svg'
+import iconeRegistrar from '../../Image/registrar.svg'
+import iconeRelatorio from '../../Image/relatorio.svg'
+
 
 const Sidebar = () => {
-    const iconMap = {
-        FaUtensils: <FaUtensils />,
-        FaBox: <FaBox />,
-        FaShoppingCart: <FaShoppingCart />,
-        FaFileAlt: <FaFileAlt />,
-        FaWarehouse: <FaWarehouse />
-      };
-    return (
-        <div className="ContainerSidebar">
-            <img src={imagemUsuario} alt="Imagem do usuatio" />
-            <h2>Ola, {texts.sidebar.usuario}!</h2>
-            <ul>
+  const iconMap = {
+    alimento: iconeAlimento,
+    compra: iconeCompra,
+    estoque: iconeEstoque,
+    registrar: iconeRegistrar,
+    relatorio: iconeRelatorio
+  };
+
+  return (
+    <div className="ContainerSidebar">
+      <div className="moldura">
+        <img src={imagemUsuario} alt="Imagem do usuário" />
+      </div>
+      <h2>Olá, {texts.sidebar.usuario}!</h2>
+      <ul>
         {texts.sidebar.menus.map((menu, index) => (
           <li key={index}>
-            <span className="icon">{iconMap[menu.icon]}</span>
+            <span className="icon">
+              <img src={iconMap[menu.icon]} alt={menu.label} />
+            </span>
             {menu.label}
           </li>
         ))}
       </ul>
-        </div>
-    )
+    </div>
+  );
 }
 
 export default Sidebar;
