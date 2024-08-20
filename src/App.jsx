1fcbc9a27c2785dc2418977ba1a574f7ef428581
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-08-20 12:21:04 -0300

    feat: implementei a tela de login para mobile e desktop usando modelo do Uiverse

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Registro from './assets/Pages/Registros';
 import NotFound from './assets/Pages/NotFound';
 import Home from './assets/Pages/Home';
 import '../src/assets/Styles/_styleBase.scss'
+import Login from './assets/Pages/Login/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/Home" element={<Home />} />
         <Route path="/Registro" element={<Registro />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
