80d6e04e65e0f26caaabebcfb1e97b57d7832937
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-08-29 20:23:50 -0300

    criando o componente loading

---
+++ src/assets/Components/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+'./_loading.scss'
+
+const Loading = () =>{
+    return(
+        <div className="container-loading">
+            <div class="custom-loader"></div>
+        </div>
+    )
+}
+
+export default Loading;
